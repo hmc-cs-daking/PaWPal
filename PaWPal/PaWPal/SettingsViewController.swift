c499aac6941019296ab7c9b4f028ee3d4720f83b
Daniel King <daniel.king2014@gmail.com>
2016-09-14 21:40:52 -0700

    Basic time pickers

--- PaWPal/PaWPal/SettingsViewController.swift
+++ PaWPal/PaWPal/SettingsViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet weak var wakeTextField: UITextField!
+    @IBOutlet weak var sleepTextField: UITextField!
+    var wakeTimePicker = UIDatePicker();
+    var sleepTimePicker = UIDatePicker();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let wakeToolBar = createToolBar(#selector(SettingsViewController.wakeDonePressed));
+        wakeTextField.inputAccessoryView = wakeToolBar;
+        wakeTextField.inputView = wakeTimePicker;
+        let sleepToolBar = createToolBar(#selector(SettingsViewController.sleepDonePressed));
+        sleepTextField.inputAccessoryView = sleepToolBar;
+        sleepTextField.inputView = sleepTimePicker;
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +30,48 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func wakeDonePressed(sender: UIBarButtonItem) {
+        wakeTextField.text = wakeTimePicker.date.description;
+        wakeTextField.resignFirstResponder()
+        
+    }
+    
+    func sleepDonePressed(sender: UIBarButtonItem) {
+        sleepTextField.text = sleepTimePicker.date.description;
+        sleepTextField.resignFirstResponder();
+    }
+    
+    func createToolBar(pressedFunc: Selector) -> UIToolbar {
+        let toolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
+    
+        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+    
+        toolBar.barStyle = UIBarStyle.BlackTranslucent
+    
+        toolBar.tintColor = UIColor.whiteColor()
+    
+        toolBar.backgroundColor = UIColor.blackColor()
+    
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: pressedFunc)
+    
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: self, action: nil)
+    
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width / 3, height: self.view.frame.size.height))
+    
+        label.font = UIFont(name: "Helvetica", size: 12)
+    
+        label.backgroundColor = UIColor.clearColor()
+    
+        label.textColor = UIColor.whiteColor()
+    
+        label.textAlignment = NSTextAlignment.Center
+    
+        let textBtn = UIBarButtonItem(customView: label)
+    
+        toolBar.setItems([flexSpace,textBtn,flexSpace,doneButton], animated: true)
+        
+        return toolBar;
+    }
     /*
     // MARK: - Navigation
 
