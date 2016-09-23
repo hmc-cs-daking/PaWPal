f6c05419d9e7cbb035c21de75e2cd1ad4e63ae0f
ameliasheppard <ameliasheppard1@gmail.com>
2016-09-23 10:16:54 -0700

    separated survey and question classes into their own files

---
+++ PaWPal/PaWPal/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  PaWPal
+//
+//  Created by Amelia Sheppard on 9/23/16.
+//  Copyright © 2016 HMC CS121 Group 5. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    var questionType = ""
+    var questionPrompt = ""
+    
+    var answerType = ""
+    //ex. slider, text, check boxes, multiple sliders
+}
---
+++ PaWPal/PaWPal/Survey.swift
@@ -0,0 +1,16 @@
+//
+//  Survey.swift
+//  PaWPal
+//
+//  Created by Amelia Sheppard on 9/23/16.
+//  Copyright © 2016 HMC CS121 Group 5. All rights reserved.
+//
+
+import Foundation
+
+
+class Survey{
+    //make this into an array of question arrays (one per survey page)
+    var questions = [Question]()
+}
+
