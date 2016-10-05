63f2a5ed0664caa435d0bba5a79b75dc9f05a538
tiffanyfong <tiffanyfong42@gmail.com>
2016-10-04 22:29:22 -0700

    Close #83 MultiCheckQustion

---
+++ PaWPal/PaWPal/MultiCheckQuestion.swift
@@ -0,0 +1,31 @@
+//
+//  MultiCheckQuestion.swift
+//  PaWPal
+//
+//  Created by Tiffany Fong on 10/4/16.
+//  Copyright © 2016 HMC CS121 Group 5. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MultiCheckQuestion: UIView {
+    
+    @IBOutlet weak var promptLabel: UILabel!
+    
+    @IBOutlet weak var answerSwitch1: UISwitch!
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var answerSwitch2: UISwitch!
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var answerSwitch3: UISwitch!
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var answerSwitch4: UISwitch!
+    @IBOutlet weak var label4: UILabel!
+    
+    @IBOutlet weak var answerSwitch5: UISwitch!
+    @IBOutlet weak var label5: UILabel!
+    
+}
