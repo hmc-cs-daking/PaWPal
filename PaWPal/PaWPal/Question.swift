c68653e11e94b345532391b6b6f5b3b93031cacb
tiffanyfong <tiffanyfong42@gmail.com>
2016-10-06 20:44:02 -0700

    #97 make 'abstract' Question class - in progress

--- PaWPal/PaWPal/Question.swift
+++ PaWPal/PaWPal/Question.swift
@@ -2,16 +2,28 @@
 //  Question.swift
 //  PaWPal
 //
-//  Created by Tiffany Fong on 9/25/16.
+//  Created by Tiffany Fong on 10/6/16.
 //  Copyright © 2016 HMC CS121 Group 5. All rights reserved.
 //
 
 import Foundation
+import UIKit
 
-class Question{
-    let prompt: String // i.e. "Where are you?"
+// Question class - subclass of UIView
+// Every question has a prompt, but subclasses will determine the answer format
+
+// Subclasses: TextQuestion, SliderQuestion, MultiCheckQuestion, MultiSliderQuestion
+// Each subclass has a corresponding xib file for the UIView
+
+class Question: UIView {
+    var prompt: String
     
     init(prompt: String) {
         self.prompt = prompt
+        super.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100)) // not sure how big this turns out
+    }
+    
+    required init?(coder aDecoder: NSCoder) { // can't make Question without this
+        fatalError("init(coder:) has not been implemented");
     }
 }
