cb8f816db1c404180bd89697e71cd68773406c16
Daniel King <daniel.king2014@gmail.com>
2016-11-01 23:44:29 -0700

    Fixing pods stuff

--- PaWPal/Pods/Target Support Files/Pods-PaWPal/Pods-PaWPal-resources.sh
+++ PaWPal/Pods/Target Support Files/Pods-PaWPal/Pods-PaWPal-resources.sh
@@ -79,16 +79,6 @@ EOM
       ;;
   esac
 }
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "FirebaseInvites/Resources/frameworks/FirebaseInvites.framework/Resources/GINInviteResources.bundle"
-  install_resource "FirebaseInvites/Resources/frameworks/FirebaseInvites.framework/Resources/GPPACLPickerResources.bundle"
-  install_resource "GoogleSignIn/Resources/GoogleSignIn.bundle"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "FirebaseInvites/Resources/frameworks/FirebaseInvites.framework/Resources/GINInviteResources.bundle"
-  install_resource "FirebaseInvites/Resources/frameworks/FirebaseInvites.framework/Resources/GPPACLPickerResources.bundle"
-  install_resource "GoogleSignIn/Resources/GoogleSignIn.bundle"
-fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
