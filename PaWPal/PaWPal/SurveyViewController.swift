88c0e2a060c07ff26c415ac95382161e94503f95
Daniel King <daniel.king2014@gmail.com>
2016-11-02 18:52:08 -0700

    Remove call from viewDidLoad

--- PaWPal/PaWPal/SurveyViewController.swift
+++ PaWPal/PaWPal/SurveyViewController.swift
@@ -15,16 +15,10 @@ class SurveyViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        if (NotificationScheduler.canTakeSurvey()) {
-            startSurveyButton.enabled = true
-        } else {
-            startSurveyButton.backgroundColor = UIColor.grayColor()
-            startSurveyButton.enabled = false
-        }
     }
     
     override func viewWillAppear(animated: Bool) {
+        print("viewWillAppear")
         super.viewWillAppear(animated) // No need for semicolon
         if (NotificationScheduler.canTakeSurvey()) {
             startSurveyButton.enabled = true
