f9d22d83e69ccea37e305e85449f9a57759eb1f7
Daniel King <daniel.king2014@gmail.com>
2016-11-09 14:44:42 -0800

    autocomplete class added

--- PaWPal/PaWPal/TextQuestion.swift
+++ PaWPal/PaWPal/TextQuestion.swift
@@ -21,8 +21,6 @@ class TextQuestion: UIView {
         textQuestion.promptLabel.text = question
         textQuestion.answerTextField.text = (AppState.sharedInstance.surveyList[key] as? String)!
         textQuestion.answerTextField.attributedPlaceholder = NSAttributedString(string: placeHolder)
-        
-        textQuestion.answerTextField.autoCompleteStrings = ["Platt", "Place", "Shanahan", "Atwood"]
         return textQuestion
         
     }
